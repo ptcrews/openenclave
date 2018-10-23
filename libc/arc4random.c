1168d430a7862e7e01fe073a41fb362c2c96c28d
vishnumohan1991 <vishnu.mohan@vvdntech.in>
2018-10-23 13:52:39 +0530

    Making single implementation for random functionality at common/rand.c

--- libc/arc4random.c
+++ libc/arc4random.c
@@ -5,6 +5,7 @@
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #include <immintrin.h>
 #include <stdlib.h>
+#include "../common/common.h"
 /*
  * Random implementation needed by libcxx as alternative to device oriented
  * randomness (/dev/rand)
@@ -13,8 +14,6 @@
 unsigned int arc4random(void)
 {
     unsigned int r;
-
-    while (!_rdrand32_step(&r))
-        ;
+    r = (unsigned int)_rdrand();
     return r;
 }
